7fab16b3dba670b4a03e901bf1364fb0e24be6db
cablll <111040151+cablll@users.noreply.github.com>
2024-04-13 16:45:00 -0700

    started creating dummy data in the db maker thing

--- studentdatabase_2.sql
+++ studentdatabase_2.sql
@@ -1,22 +1,25 @@
 #create schema `studentdb_test`;
 
+# Dropping the tables for ease of reset
+#DROP TABLE `studentdb_test`.`course`, `studentdb_test`.`department`, `studentdb_test`.`instructor`, `studentdb_test`.`studentcourse`, `studentdb_test`.`studentinfo`;
+
 create table `studentinfo` (
     `studentid` int not null AUTO_INCREMENT,
 	`password`  varchar(50) not null,
     `fname` varchar(50) null,
     `lname` varchar(50) null,
-    `phonenum` int null,
+    `phonenum` varchar(50) null,
     `DOB` varchar(50) null,
     `gender` varchar(50) null,
     primary key (`studentid`));
     
 create table `department` (
-    `deptid` int not null,
+    `deptid` int not null AUTO_INCREMENT,
     `deptname` varchar(50) null,
     primary key (`deptid`));
     
 create table `course` (
-    `courseid` int not null,
+    `courseid` int not null AUTO_INCREMENT,
     `cname` varchar(50) null,
     `instructor` varchar(50) null,
     `deptid` int null,
@@ -24,7 +27,7 @@ create table `course` (
     foreign key (`deptid`) references department(`deptid`));
     
 create table `instructor` (
-    `iid` int not null,
+    `iid` int not null AUTO_INCREMENT,
     `name` varchar(50) null,
     `email` varchar(50) null,
     `phone` int null,
@@ -38,5 +41,10 @@ create table studentcourse (
     primary key (studentid, courseid),
     foreign key (studentid) references studentinfo(studentid),
     foreign key (courseid) references course(courseid));
-    
-INSERT INTO `studentdb_test`.`studentinfo` (`studentid`,`password`, `fname`, `lname`, `phonenum`, `DOB`, `gender`) VALUES (0001,'Green', 'Mario', 'Mario', 555-555-5555, 3023, 'Male');
+
+# creating dummy data
+INSERT INTO `studentdb_test`.`studentinfo` (`password`, `fname`, `lname`, `phonenum`, `DOB`, `gender`) VALUES ('Green', 'Mario', 'Mario', '555-555-5555', '1/2/3023', 'Male');
+INSERT INTO `studentdb_test`.`studentinfo` (`password`, `fname`, `lname`, `phonenum`, `DOB`, `gender`) VALUES ('Blue', 'Toad', 'Blue', '555-555-5555', '5/12/100', 'Toad');
+INSERT INTO `studentdb_test`.`studentinfo` (`password`, `fname`, `lname`, `phonenum`, `DOB`, `gender`) VALUES ('Luigi', 'Luigi', 'Mario', '555-555-5555', '1/1/2023', 'Luigi');
+
+
